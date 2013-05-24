6c8552167f7e08e2c1d67a39a73f31fdb303e8f5
Diosney <diosney.s@gmail.com>
2013-05-24 14:07:30 -0400

    Basic/Tables: Added basic support for simple tables.

--- routes/basic/tables.js
+++ routes/basic/tables.js
@@ -1,4 +1,10 @@
 module.exports 	= {
+	simple       : function (req, res) {
+		res.render('basic/tables/simple', {
+			title: 'Express',
+			page: 'basic/tables/simple'
+		});
+	},
 	data       : function (req, res) {
 		res.render('basic/tables/data', {
 			title: 'Express',
