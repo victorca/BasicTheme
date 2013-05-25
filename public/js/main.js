f9a7fdbfaf9654bd6db3e93589f845dd726cb5ec
Diosney <diosney.s@gmail.com>
2013-05-25 12:34:41 -0400

    Forms/Bcasi: Added basic support for uniform fields.

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,7 @@
-
+(function ($) {
+	/*
+	 Uniform fields.
+	 */
+	$('form.uniform input,form.uniform select, form.uniform .textarea')
+		.add('.uniform.input,.uniform.select, .uniform.textarea').uniform();
+})(jQuery);
