bf224d97cdf1189354d6b85ae2dd8319c8a86b13
Diosney <diosney.s@gmail.com>
2013-05-25 15:27:57 -0400

    Forms/Basic: Fixed checkboxes & radios styles.

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,9 @@
 (function ($) {
 	/*
 	 Uniform fields.
+	 TODO: Add a filter option to fields to be excluded.
 	 */
 	$('form.uniform input,form.uniform select, form.uniform .textarea')
-		.add('.uniform.input,.uniform.select, .uniform.textarea').uniform();
+		.add('.uniform.input,.uniform.select, .uniform.textarea')
+		.uniform();
 })(jQuery);
