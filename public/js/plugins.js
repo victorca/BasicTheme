86f49570f9f16cda25da1b2ea2b2694b4291503b
Diosney <diosney.s@gmail.com>
2013-06-06 17:44:23 -0400

    Form/Advanced: Added basic support for Daterange pickers.

--- public/js/plugins.js
+++ public/js/plugins.js
@@ -55,21 +55,75 @@ window.prettyPrint && prettyPrint();
 /*
  Auto-updated progress bars.
  */
-$('.progress-preview').each(function(){
+$('.progress-preview').each(function () {
 	var el = $(this);
-	function up(){
+
+	function up() {
 		var width = el.find('.bar').width();
 		var parentWidth = el.width();
-		var percent = Math.round((100*width/parentWidth));
+		var percent = Math.round((100 * width / parentWidth));
 		var plus = Math.round(Math.random() * 10);
 		var newPercent = percent + plus;
-		if(newPercent > 100){
+		if (newPercent > 100) {
 			newPercent = 0;
 		}
-		el.find('.bar').width(newPercent+"%").html(newPercent+"%");
+		el.find('.bar').width(newPercent + "%").html(newPercent + "%");
 		setTimeout(up, 1500);
 	}
+
 	up();
+});
+
+/*
+ Daterange pickers.
+ */
+$('#daterangepicker-simple').daterangepicker();
+
+$('#daterangepicker-dropdown').daterangepicker({
+	minDate      : '01/01/2010',
+	maxDate      : '12/31/2015',
+	showDropdowns: true
+});
+
+$('#daterangepicker-range').daterangepicker({
+	dateLimit: { days: 3 }
+});
+
+$('#daterangepicker-reportrange').daterangepicker({
+		ranges         : {
+			'Today'       : [new Date(), new Date()],
+			'Yesterday'   : [moment().subtract('days', 1), moment().subtract('days', 1)],
+			'Last 7 Days' : [moment().subtract('days', 6), new Date()],
+			'Last 30 Days': [moment().subtract('days', 29), new Date()],
+			'This Month'  : [moment().startOf('month'), moment().endOf('month')],
+			'Last Month'  : [moment().subtract('month', 1).startOf('month'), moment().subtract('month', 1).endOf('month')]
+		},
+		opens          : 'left',
+		format         : 'MM/DD/YYYY',
+		separator      : ' to ',
+		startDate      : moment().subtract('days', 29),
+		endDate        : new Date(),
+		minDate        : '01/01/2012',
+		maxDate        : '12/31/2013',
+		locale         : {
+			applyLabel      : 'Submit',
+			fromLabel       : 'From',
+			toLabel         : 'To',
+			customRangeLabel: 'Custom Range',
+			daysOfWeek      : ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa'],
+			monthNames      : ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+			firstDay        : 1
+		},
+		showWeekNumbers: true,
+		buttonClasses  : ['btn-danger'],
+		dateLimit      : false
+	},
+	function (start, end) {
+		$('#daterangepicker-reportrange span').html(start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'));
+	}
+);
+//Set the initial state of the picker label
+$('#daterangepicker-reportrange span').html(moment().subtract('days', 29).format('MMMM D, YYYY') + ' - ' + moment().format('MMMM D, YYYY'));
 });/*
  Add support messaging
  */
