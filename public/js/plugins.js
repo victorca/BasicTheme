25af772fe46665c8f9a26df5c8135145ec029e45
Diosney <diosney.s@gmail.com>
2013-06-06 19:32:56 -0400

    Form/Advanced: Added basic support for Knobs.

--- public/js/plugins.js
+++ public/js/plugins.js
@@ -124,7 +124,24 @@ $('#daterangepicker-reportrange').daterangepicker({
 );
 //Set the initial state of the picker label
 $('#daterangepicker-reportrange span').html(moment().subtract('days', 29).format('MMMM D, YYYY') + ' - ' + moment().format('MMMM D, YYYY'));
-});/*
+
+/*
+ Knobs.
+ */
+function clock() {
+	var $s = $(".second"),
+		$m = $(".minute"),
+		$h = $(".hour");
+	d = new Date(),
+		s = d.getSeconds(),
+		m = d.getMinutes(),
+		h = d.getHours();
+	$s.val(s).trigger("change");
+	$m.val(m).trigger("change");
+	$h.val(h).trigger("change");
+	setTimeout("clock()", 1000);
+}
+clock();});/*
  Add support messaging
  */
 (function ($) {
