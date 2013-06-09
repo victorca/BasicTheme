3f87b7018d3e18e65a0c56ba7ae8eb329a9e41f1
Diosney <diosney.s@gmail.com>
2013-06-09 18:48:03 -0400

    Progress bars: Moved from Forms/Advanced to Basic/Widgets/Progress bars.

--- routes/basic/widgets.js
+++ routes/basic/widgets.js
@@ -1,4 +1,10 @@
 module.exports = {
+	progressbar: function (req, res) {
+		res.render('basic/widgets/progressbar', {
+			title: 'Express',
+			page : 'basic/widgets/progressbar'
+		});
+	},
 	conversations: function (req, res) {
 		res.render('basic/widgets/conversations', {
 			title: 'Express',
--- routes/index.js
+++ routes/index.js
@@ -26,6 +26,7 @@ module.exports = function (app) {
 
 	app.get('/basic/images/icons', require('./basic/images.js').icons);
 
+	app.get('/basic/widgets/progressbar', require('./basic/widgets.js').progressbar);
 	app.get('/basic/widgets/conversations', require('./basic/widgets.js').conversations);
 
 	/*
