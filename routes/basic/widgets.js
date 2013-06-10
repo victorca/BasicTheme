b668f501d51c11d10ae4e98486fbb6f9f2c083d5
Diosney <diosney.s@gmail.com>
2013-06-10 17:13:30 -0400

    Basic/Widgets: Added Box section.

--- routes/basic/widgets.js
+++ routes/basic/widgets.js
@@ -1,4 +1,10 @@
 module.exports = {
+	boxes: function (req, res) {
+		res.render('basic/widgets/boxes', {
+			title: 'Express',
+			page : 'basic/widgets/boxes'
+		});
+	},
 	progressbar: function (req, res) {
 		res.render('basic/widgets/progressbar', {
 			title: 'Express',
